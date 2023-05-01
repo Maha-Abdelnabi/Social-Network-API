786be14b8919cd0deafc7a88d7f49b5968151c4f
Maha Abdelnabi <mahaomar@gmail.com>
2023-04-30 23:22:26 -0400

    create db mongoose connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const db = require("./config/connection");
+//const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,4 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+db.once("open", () => {
+ app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+ });
+});
